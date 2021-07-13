d3877aa4223d4a5aaba9ed54fddf14a75e9e66e9
Pawel Borkar <paawel@pm.me>
2021-07-13 15:29:47 +0530

    Fix About Tabs

--- script.js
+++ script.js
@@ -61,18 +61,6 @@ document.addEventListener("click", (e) => {
 
 /*------------------------------------About Tabs-------------------------------------*/
 
-const tabsContainer = document.querySelector(".about-tabs"),
-aboutSection = document.querySelector(".about-section");
-
-tabsContainer.addEventListener("click",(e) => {
-    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
-        tabsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
-        let target = e.target.getAttribute("data-target");
-        aboutSection.querySelector(".tab-content.active").classList.remove("active");
-        aboutSection.querySelector(target).classList.add("active");
-    }
-});
 
 /*----------------------------Portfolio Item Details PopUp----------------------------*/
 
